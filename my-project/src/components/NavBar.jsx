86793407fae00127889cf265e172a647364b9c9c
matthewkuria <mathewkwachira@gmail.com>
2024-01-01 23:13:42 +0300

    add a hover effect to the links

--- my-project/src/components/NavBar.jsx
+++ my-project/src/components/NavBar.jsx
@@ -2,17 +2,18 @@ import React from "react";
 
 export default function NavBar (){
     return(
-        <section className="NavBar flex justify-between">
-            <h3 className="font-bold">Matthew Kuria</h3>
+        <section className="NavBar container flex items-center justify-between bg-gray-300 h-20 w-full">
+            <h3 className="font-bold mx-3">Matthew Kuria</h3>
             <div className="hidden md:flex">
                 <ul className="flex justify-between">
-                    <li className="mx-5"><a href="">Home</a></li>
-                    <li className="mx-5"><a href="">About</a></li>
-                    <li className="mx-5"><a href="">portifolio</a></li>
-                    <li className="mx-5"><a href="">Skills</a></li>
-                    <li className="mx-5"><a href="">Contact</a></li>
+                    <li className="mx-5"><a href="" className="hover:font-semibold">Home</a></li>
+                    <li className="mx-5"><a href="" className="hover:font-semibold">About</a></li>
+                    <li className="mx-5"><a href="" className="hover:font-semibold">portifolio</a></li>
+                    <li className="mx-5"><a href="" className="hover:font-semibold">Skills</a></li>
+                    <li className="mx-5"><a href="" className="hover:font-semibold">Contact</a></li>
                 </ul>
             </div>
+            <img src="../icon-hamburger.svg" alt="" className=" block md:hidden p-6 hover:bg-black"/>
         </section>
     )
 }
