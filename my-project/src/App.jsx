b21ce5726b835a84a549a38c9109e2e7bed0dbad
matthewkuria <mathewkwachira@gmail.com>
2024-01-03 00:19:49 +0300

    add a form section for the mobile design

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -5,9 +5,11 @@ import About from './components/About'
 import ProjectCard from './components/ProjectCard'
 import Skills from './components/Skills'
 import './App.css'
+import {useForm} from "react-hook-form";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { register, handleSubmit } = useForm();
+  const onSubmit = data => alert(data);
 
   return (
     <>
@@ -18,6 +20,17 @@ function App() {
       <ProjectCard />
     </div>
     <Skills />
+    <section className="FORM bg-cyan-400 h-72">
+            <h1>Leave a Message </h1>
+            <form onSubmit={handleSubmit(onSubmit)} > 
+
+                    <input {...register("email")}  />          
+                
+                    <input {...register("message")}/>
+              
+                <button type="submit" className="bg-green-500 border-2  hover:animate-none border-green-500 hover:bg-white mx-4 px-3 py-2 rounded hover:text-green-500">Submit</button>
+            </form>
+        </section>
       
     </>
   )
