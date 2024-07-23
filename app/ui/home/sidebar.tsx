81959244e7b07f8388255d3f86602890f53325bd
matthewkuria <mathewkwachira@gmail.com>
2024-07-23 23:35:25 +0300

    Make it hidden for small screens

--- app/ui/home/sidebar.tsx
+++ app/ui/home/sidebar.tsx
@@ -11,7 +11,7 @@ export default function SideBar() {
                     coupled with back end hands-on craftmanship.
                 </p>
             </div>
-            <div className="Socio-icons animate-bounce md:animate-none  flex items-center justify-center  md:bottom-44 mt-6 md:mt-12  md:fixed  ">
+            <div className="hidden Socio-icons animate-bounce md:animate-none  md:flex items-center justify-center  md:bottom-44 mt-6 md:mt-12  md:fixed  ">
                 <div className=" hover:animate-pulse">
                 <Link href="https://twitter.com/matthew_kuria" className=" "><svg xmlns="http://www.w3.org/2000/svg" height="32" width="32" viewBox="0 0 512 512"><path fill="#ffff" d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg></Link>
                 </div>
