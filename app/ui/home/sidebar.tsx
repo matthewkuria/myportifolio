fe2e7acc169fc2ecbbfc51660c52505e6e0e448a
Matthewkuria <mathewkwachira@gmail.com>
2025-01-02 20:50:22 +0300

    Fix the sidebar and background color of my portfolio

--- app/ui/home/sidebar.tsx
+++ app/ui/home/sidebar.tsx
@@ -4,9 +4,9 @@ import NavLinks from "./nav-links";
 export default function SideBar() {
     return (
         <main className="text-white flex h-full flex-col px-2 py-4 md:px-2">            
-            <div className="max-w-96">
+            <div className="max-w-96 md:max-w-sm">
                 <h1 className="text-4xl mt-10 font-bold">Matthew Kuria</h1>
-                <p className="my-2 text-2xl text-sky-500 font-bold">Full Stack Engineer</p>
+                <p className="my-2 text-2xl text-yellow-400 font-bold">Software Developer</p>
                 <p className="text-sm my-3">I have a vast experience building pixel-perfect and responsive User Interfaces,
                     coupled with back end hands-on craftmanship.
                 </p>
