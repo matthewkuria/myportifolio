9f2622a30bd5b7dbca56cca4d2ec31f2c7ff867e
Matthewkuria <mathewkwachira@gmail.com>
2025-01-25 09:09:07 +0300

    Add the skills to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,10 @@ export default function Home() {
   return (
     <main className="intro text-white bg-gradient-to-r from-cyan-500 to-blue-500 flex flex-col md:flex md:justify-center md:items-center p-5 min-h-[60vh] md:min-h-[100vh]">
       <div className="text-left">
-        <h4 className="font-semibold text-4xl">Welcome to my portfolio</h4>
+        <h1 className="font-semibold text-4xl">Welcome to my portfolio</h1>
         <p className="my-2">I am a Software Developer who is passionate about building web applications that are user-friendly and accessible.</p>
         <Link href="/about" className="text-yellow-500 hover:text-white">Read More...</Link>
+        <h2 className="font-semibold text-3xl">My Skills</h2>
       </div>
            
     </main>
