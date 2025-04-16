df64c4acb48f9ff731536ba6ec66ec38e593b180
Matthewkuria <mathewkwachira@gmail.com>
2025-04-16 05:33:48 +0300

    Add hover effect to the social icons

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -3,11 +3,26 @@ import { SocialIcon } from "react-social-icons";
 export default function Socials() {
     return (
         <div className="socials h-6 p-2 items-center justify-between text-white w-full ">
-        <div className="flex gap-4 md:gap-10 items-center justify-around md:justify-between  w-full">
-          <SocialIcon url="https://linkedin.com/" href="https://linkedin.com/in/matthew-kuria" style={{ height: 25, width: 25 }}/>
-            <SocialIcon url="https://github.com" href="https://github.com/matthewkuria" style={{ height: 25, width: 25 }}/>            
-            <SocialIcon url="https://twitter.com" href="https://twitter.com/matthew_kuria" style={{ height: 25, width: 25 }}/>
-          </div>
+        <div className="flex gap-4 md:gap-10 items-center justify-around md:justify-between w-full">
+            <SocialIcon 
+          url="https://linkedin.com/" 
+          href="https://linkedin.com/in/matthew-kuria" 
+          style={{ height: 25, width: 25 }} 
+          className=" opacity-75 rounded-3xl p-4  hover:opacity-100 transition-opacity duration-200"
+            />
+            <SocialIcon 
+          url="https://github.com" 
+          href="https://github.com/matthewkuria" 
+          style={{ height: 25, width: 25 }} 
+          className=" opacity-75 rounded-3xl p-4  hover:opacity-100 transition-opacity duration-200"
+            />            
+            <SocialIcon 
+          url="https://twitter.com" 
+          href="https://twitter.com/matthew_kuria" 
+          style={{ height: 25, width: 25 }} 
+          className=" opacity-75 rounded-3xl p-4  hover:opacity-100 transition-opacity duration-200"
+            />
+        </div>
           
         </div>
     )
