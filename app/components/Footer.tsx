55d7de749343667c59f30e6054129103e5a379ba
Matthewkuria <mathewkwachira@gmail.com>
2025-04-16 04:00:33 +0300

    Add a footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 
 export default function Footer() {
     return (
-        <footer className="footer bg-slate-600 text-white text-base" id="get-in-touch">
+        <footer className="footer text-white text-base" id="get-in-touch">
+            <div className="footer-content flex flex-col items-center justify-center py-10">
+                
+            </div>
             
-            <div className="copyright text-xs font-semibold flex justify-center bg-black py-2">
+            <div className="copyright text-xs font-semibold md:font-normal flex justify-center bg-black py-2">
                 <p className="tracking-wide">&copy; copyright-{new Date().getFullYear()} || MK || All rights reserved</p>
             </div>
         </footer>
