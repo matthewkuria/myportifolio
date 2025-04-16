6f2998b3836fea581d462fb9f4a53a4f46b6f844
Matthewkuria <mathewkwachira@gmail.com>
2025-04-16 05:26:52 +0300

    Customize the social media links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import {  open_sans } from "../fonts/fonts";
 import MkLogo from "./MkLogo";
+import Socials from "./Socials";
 
 export default function Footer() {
     return (
@@ -9,7 +10,7 @@ export default function Footer() {
                 <MkLogo />
             </div>
             <div className="footer-content flex flex-col md:flex-row justify-between md:justify-around items-baseline p-4 md:p-8">
-                <div className="w-full md:w-1/3 flex flex-col items-start justify-center text-left">
+                <div className="w-full md:w-1/2 flex flex-col items-start justify-center text-left mt-10">
                     <h4 className={` footer-title`}>Quick Links</h4>
                     <ul className="list-none">
                         <li><Link href="/">Home</Link></li>
@@ -18,12 +19,24 @@ export default function Footer() {
                         <li><Link href="#">Resume</Link></li>
                     </ul>
                 </div>
-                <div className="w-full md:w-1/3 flex items-center justify-center">
+                <div className="w-full md:w-1/3 flex flex-col items-start justify-center text-left">
                     <h5 className="footer-title">Contact Me at:</h5>
-                    
+                    <ul>
+                        <li>
+                            <Link href="mailto:mathewkwachira@gmail.com">
+                                 mathewkwachira@gmail.com
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="tel:0718421871">
+                                0718421871
+                            </Link>
+                        </li>
+                    </ul>                    
                 </div>
-                <div className="w-full md:w-1/3 flex items-end justify-end text-right">
+                <div className="w-full md:w-1/4">
                     <h6 className="footer-title">Follow Me On:</h6>
+                    <Socials />
                 </div>
             </div>
             <div className="copyright text-xs font-semibold md:font-normal flex justify-center bg-black py-2">
--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -2,13 +2,11 @@ import { SocialIcon } from "react-social-icons";
 
 export default function Socials() {
     return (
-        <div className="socials bg-blue-900 h-6 p-2 items-center justify-between text-white w-full hidden md:flex">
-          <div className="flex gap-3">
-          <p className="">Follow Me</p>
-            <SocialIcon url="https://facebook.com" href="https://facebook.com/" style={{ height: 20, width: 20 }}/>
-            <SocialIcon url="https://youtube.com/" style={{ height: 20, width: 20 }}/>
-            <SocialIcon url="https://twitter.com" style={{ height: 20, width: 20 }}/>
-            <SocialIcon url="https://LinkedIn.com" style={{ height: 20, width: 20 }}/>
+        <div className="socials h-6 p-2 items-center justify-between text-white w-full ">
+        <div className="flex gap-4 md:gap-10 items-center justify-around md:justify-between  w-full">
+          <SocialIcon url="https://linkedin.com/" href="https://linkedin.com/in/matthew-kuria" style={{ height: 25, width: 25 }}/>
+            <SocialIcon url="https://github.com" href="https://github.com/matthewkuria" style={{ height: 25, width: 25 }}/>            
+            <SocialIcon url="https://twitter.com" href="https://twitter.com/matthew_kuria" style={{ height: 25, width: 25 }}/>
           </div>
           
         </div>
