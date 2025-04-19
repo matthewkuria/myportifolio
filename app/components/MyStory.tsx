aa5fa03b530fb584578f4bcb1e7a8c76685928e6
Matthewkuria <mathewkwachira@gmail.com>
2025-04-19 21:35:51 +0300

    More changes

--- app/components/MyStory.tsx
+++ app/components/MyStory.tsx
@@ -2,19 +2,22 @@ import React from 'react'
 import Image from 'next/image'
 import GetInTouchButton from './GetInTouchButton'
 import mattImage from "../../public/matt.jpg"
+import Socials from './Socials'
 
 const MyStory = () => {
   return (
-    <div className="flex flex-col justify-center md:flex-row w-full h-[350px] bg-secondary p-4 md:p-8 my-5">
+    <div className="flex flex-col justify-center items-center md:flex-row w-full md:h-[350px] bg-secondary p-4 md:p-8 my-5">
         <div className="md:mx-5">
-              <Image src={mattImage} width={200} height={300}  alt='Matthew Kuria'/>
+              <Image src={mattImage} width={200} height={300}  alt='Matthew Kuria'
+               className='rounded-full md:rounded-none h-[200px] w-auto md:h-[300px]'
+              />
         </div>
           <div className="flex flex-col w-full md:w-3/4">
               <div className="">
                   <h1 className="text-background text-2xl md:text-3xl font-bold">About Matthew Kuria</h1>
-                  <p className="text-background text-md md:text-lg">I am a prolific software developer.</p>
+                  <p className="text-background text-md md:text-lg">I am a prolific <span className='text-yellow-500 font-semibold'>frontend developer.</span></p>
                   <p className="text-background text-md">I have a strong foundation in HTML, CSS, and JavaScript.
-                    I have experience working with various frameworks and libraries, including React and Next.js.
+                    I have experience working with various frameworks and libraries, including React-Next.js, Tailwind CSS and TypeScript.
                     I am always eager to learn new technologies and improve my skills.
                     I am a problem solver at heart and enjoy tackling challenges head-on.
                     I believe that good communication and collaboration are key to successful projects,
@@ -26,8 +29,11 @@ const MyStory = () => {
                     let's connect and see how we can work together!
                       </p>
               </div>
-              <div className=" my-5">
-                  <GetInTouchButton />
+              <div className="flex flex-col md:flex-row md:items-center md:justify-center my-5 w-full md:w-1/2 ">
+                 <div className="w-1/2 mt-5">
+                   <GetInTouchButton />
+                 </div>
+                  <Socials />
               </div>
         </div>
           
