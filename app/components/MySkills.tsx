6f73154a58c1bc7b19b503ab5c216d5e9bc9c3b6
Matthewkuria <mathewkwachira@gmail.com>
2025-05-06 15:43:37 +0300

    More changes on the skills section

--- app/components/MySkills.tsx
+++ app/components/MySkills.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const MySkills = () => {
   return (
     <div className="bg-gray-100 p-8 rounded-lg shadow-lg">
-        <h1 className="text-gray-800 text-3xl md:text-4xl font-extrabold mb-6 text-center">My Skills</h1>
-        <div className="flex flex-col md:flex-row justify-center items-center gap-10">
-            <div className="w-full md:w-1/2 flex flex-col items-start justify-center text-left">
+        <h1 className="text-gray-800 text-3xl md:text-4xl font-extrabold mb-6 text-left md:text-center ml-10 md:ml-0">My Skills</h1>
+        <div className="flex flex-col md:flex-row justify-center items-center px-20 gap-20 ">
+            <div className="w-full md:w-1/2 flex flex-col items-start justify-center text-left md:ml-48">
               <h4 className="text-xl font-semibold text-gray-700 mb-4 border-b-2 border-gray-300 pb-2">Frontend</h4>
               <ul className="list-disc list-inside text-gray-600 space-y-2">
                 <li>HTML</li>
@@ -16,7 +16,7 @@ const MySkills = () => {
                 <li>Tailwind CSS</li>
               </ul>
             </div>
-            <div className="w-full md:w-1/2 flex flex-col items-start justify-center text-left">
+            <div className="w-full md:w-1/2 flex flex-col items-start justify-center text-left md:ml-32">
               <h4 className="text-xl font-semibold text-gray-700 mb-4 border-b-2 border-gray-300 pb-2">Backend</h4>
               <ul className="list-disc list-inside text-gray-600 space-y-2">
                 <li>Django</li>
